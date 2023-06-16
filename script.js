a226d120737f05b81e43d012312341885b0a2d85
Divyansh Singh Bhati <diviyanshbhati7774@gmail.com>
2023-06-16 19:43:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// var loginButton = document.getElementById('loginButton');
+// var loginPage = document.getElementById('loginPage');
+// var loginForm = document.getElementById('loginForm');
+
+// loginButton.addEventListener('click', function(event) {
+//   event.preventDefault(); // Prevent default link behavior
+//   loginPage.style.display = 'block';
+// });
+
+// loginForm.addEventListener('submit', function(event) {
+//   event.preventDefault(); // Prevent form submission
+
+//   // Perform login validation here
+//   var username = document.getElementById('username').value;
+//   var password = document.getElementById('password').value;
+
+//   // Check username and password
+//   if (username === 'admin' && password === 'password') {
+//     // Hide the login page
+//     loginPage.style.display = 'none';
+    
+//     // You can redirect to another page or perform any other action here
+//     console.log('Login successful!');
+//   } else {
+//     // Display an error message or perform any other action for unsuccessful login
+//     console.log('Invalid username or password');
+//   }
+// });
